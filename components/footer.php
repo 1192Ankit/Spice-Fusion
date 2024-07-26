fc91506979a93d9b4d1d5a25803992e81b42d9ee
1192Ankit <110929365+1192Ankit@users.noreply.github.com>
2024-07-26 22:55:58 +0900

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,37 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:singhankit.0713@gmail.com">singhankit.0713@gmail.com</a>
+         <a href="mailto:help@spicefusion.com">help@spicefusion.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>12:00 - 23:00</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">Howrah, India - 711109</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:033-26585057">033-26585057</a>
+         <a href="tel:+917980856836">+917980856836</a>
+      </div>
+
+   </section>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
